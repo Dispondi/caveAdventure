78b5008cc1f938dcb8c65afdf695537c4aeadd20
Krytoi Xa4 <andrey.grachev.2007@mail.ru>
2025-01-18 00:02:28 +0300

    Передвинул логику создания игры из Main в Game. Создал WinSegment. Нужно будет создать класс Player, который будет хранить в себе очки. В Game нужно будет передавать Player, чтобы Game.endGame() выводил очки, набранные игроком

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,9 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Game {
-    private final int DIFFICULT;
-    private final int WAY_LENGTH;
+    public int DIFFICULT;
+    public int WAY_LENGTH;
     private final MapGame mapGame;
 
     public Game(int d, int l) {
@@ -9,7 +12,80 @@ public class Game {
         mapGame = new MapGame();
     }
 
-    public void start() {
+    private void start() {
         new CorridorSegment(mapGame, mapGame.segmentID).playSegment();
+    }
+
+    private void printStartMenu() {
+        System.out.println();
+        System.out.println("-----------------");
+        System.out.println("Настройки игры:");
+        System.out.println("Сложность: " + DIFFICULT + "/10");
+        System.out.println("Длина пути: " + WAY_LENGTH);
+        System.out.println("Для изменения настроек, введите пункт, который хотите изменить.");
+        System.out.println("----------------\n");
+        System.out.println("Перед собой вы видите заброшенный вход в пещеру, желаете ли войти?\nДа/Нет");
+    }
+
+    public void startMenu() {
+        this.printStartMenu();
+
+        Scanner sc = new Scanner(System.in);
+        String playerMessage = sc.nextLine().toLowerCase();
+        switch (playerMessage) {
+            case "сложность": {
+                this.changeDifficult();
+                return;
+            }
+            case "длина пути": {
+                this.changeLength();
+                return;
+            }
+            case "нет": {
+                endGame();
+                return;
+            }
+            case "да": {
+                this.start();
+                return;
+            }
+        } startMenu();
+    }
+
+    private void changeDifficult() {
+        System.out.println("Введите сложность от 0 до 10:");
+
+        Scanner sc = new Scanner(System.in);
+        int playerMessage;
+        try {
+            playerMessage = sc.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Неверный ввод!");
+            changeDifficult();
+            return;
+        }
+
+        DIFFICULT = Math.min(Math.abs(playerMessage), 10);
+        startMenu();
+    }
+
+    private void changeLength() {
+        System.out.println("Введите длину пути. Вам нужно будет пройти столько клеток, чтобы добраться до выхода");
+
+        Scanner sc = new Scanner(System.in);
+        int playerMessage;
+        try {
+            playerMessage = sc.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Неверный ввод!");
+            changeLength();
+            return;
+        }
+
+        WAY_LENGTH = Math.abs(playerMessage);
+        startMenu();
+    }
+    public static void endGame() {
+        System.out.println("Вы вышли из подземелья. Конец.");
     }
 }
