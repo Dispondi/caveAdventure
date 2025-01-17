02a1c7408c51bc02130216565b17119cf0c12af4
Krytoi Xa4 <andrey.grachev.2007@mail.ru>
2025-01-17 03:35:17 +0300

    Сделал, чтобы сегменты запоминали из какого сегмента они появились. БАГ: Когда идешь вперед, сегменты не помнят, было ли что-то дальше их и создают новый сегмент

--- src/Segment.java
+++ src/Segment.java
@@ -4,19 +4,26 @@ import segmentswork.SegmentsConstants;
 import java.util.Scanner;
 
 public class Segment {
-    protected MapGame mapGame;
-    protected String description;
+    private final MapGame mapGame;
+    public final String description;
+    public final int id;
+    public final int parent_id;
 
-    protected Segment(String description, MapGame mapGame) {
+    protected Segment(String description, MapGame mapGame, int parent_id) {
         this.description = description;
         this.mapGame = mapGame;
+        this.id = mapGame.segmentID;
+        this.parent_id = parent_id;
         mapGame.addSegment(this);
     }
-    private static Segment createRandSegment(MapGame mapGame) {
+    private static Segment createRandSegment(MapGame mapGame, int parent_id) {
         int randI = SupportOperations.randInRange(0, SegmentsConstants.ALL_TYPES_SEGMENTS.length - 1);
         switch (SegmentsConstants.ALL_TYPES_SEGMENTS[randI]) {
             case SegmentsConstants.CORRIDOR_SEGMENT -> {
-                return new CorridorSegment(mapGame);
+                return new CorridorSegment(mapGame, parent_id);
+            }
+            case SegmentsConstants.FORK_SEGMENT -> {
+                return new ForkSegment(mapGame, parent_id);
             }
         }
         return null; // когда сделаю больше сегментов, здесь будет возвращаться обычный коридор
@@ -26,6 +33,7 @@ public class Segment {
         System.out.println(this.description);
         String playerMessage = sc.nextLine().toLowerCase();
         switch (playerMessage) {
+            case "вперёд":
             case "вперед": {
                 goForward();
                 return;
@@ -37,15 +45,23 @@ public class Segment {
         } this.playSegment();
     }
     public void goForward() {
-        createRandSegment(mapGame).playSegment();
+        createRandSegment(mapGame, this.id).playSegment();
     }
     public void goBackward() {
-        int i = mapGame.findNumberOfSegment(this);
-        if (i == 0) {
+//        int i = mapGame.findNumberOfSegment(this);
+//        if (i == 0) {
+//            Main.endGame();
+//        } else if (i > 0) {
+//            System.out.println("НОМЕР СЕГМЕНТА С КОТОРОГО ВЫШЕЛ: " + i);
+//            System.out.println("НОМЕР СЕГМЕНТА НА КОТОРЫЙ ПРИШЕЛ: " + (i - 1));
+//            mapGame.getSegment(i - 1).playSegment();
+//        }
+        if (this.id == 0) {
             Main.endGame();
-        } else if (i > 0) {
-            System.out.println("НОМЕР СЕГМЕНТА: " + i);
-            mapGame.getSegment(i - 1).playSegment();
+        } else {
+            System.out.println("НОМЕР СЕГМЕНТА С КОТОРОГО ВЫШЕЛ: " + this.id);
+            System.out.println("НОМЕР СЕГМЕНТА НА КОТОРЫЙ ПРИШЕЛ: " + this.parent_id);
+            mapGame.getSegment(this.parent_id).playSegment();
         }
     }
 }
--- src/segmentswork/SegmentsConstants.java
+++ src/segmentswork/SegmentsConstants.java
@@ -2,7 +2,28 @@ package segmentswork;
 
 public final class SegmentsConstants {
     public static final String CORRIDOR_SEGMENT = "corridor";
-    public static final String[] ALL_TYPES_SEGMENTS = new String[] {CORRIDOR_SEGMENT};
-    public static final String CORRIDOR_DESCRIPTION = "-----------------\nВы видите перед собой длинный коридор.\nЖелаете пройти дальше?\nВперед/Назад\n-----------------";
+    public static final String FORK_SEGMENT = "fork";
+    public static final String[] ALL_TYPES_SEGMENTS = new String[] {CORRIDOR_SEGMENT, FORK_SEGMENT};
+    public static final String CORRIDOR_DESCRIPTION = """
+            -----------------
+            Вы видите перед собой длинный коридор.
+            Желаете пройти дальше?
+            Вперед/Назад
+            -----------------""";
+    public static final String FORK_DESCRIPTION = """
+            -----------------
+            Блуждая в темном коридоре, вы замечаете свет в конце.
+            Вы переступаете через страх и подходите посмотреть.
+            """;
+    public static final String DOUBLE_FORK_OFFER = """
+            Перед вами две двери.
+            В какую зайдете?
+            Левую/Правую/Назад
+            -----------------""";
+    public static final String TRIPLE_FORK_OFFER = """
+            Перед вами три двери.
+            В какую зайдете?
+            Левую/Среднюю/Правую/Назад
+            -----------------""";
     private SegmentsConstants() {}
 }
